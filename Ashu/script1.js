66f526fc3c1237fba220ac37465c1750d0478858
ashu-0453 <86668736+ashu-0453@users.noreply.github.com>
2021-07-26 23:26:02 +0530

    Add files via upload

---
+++ Ashu/script1.js
@@ -0,0 +1,14 @@
+ //1. WAYS TO PRINT IN JS
+        //console.log("Hello World");
+        //alert("me");
+        //document.write("WRITE HERE");
+//2. JAVASCRIPT CONSOLE API
+        console.log("Hello World");
+        console.warn("This is a WARNING");
+        console.error("This is an error");
+//3. JS Variables
+var num1 = 54;
+var num2 = 36;
+console.log(num1 + num2);
+
+//4. 
